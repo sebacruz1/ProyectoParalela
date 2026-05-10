c6fdd9c262b887c967ff3ec76600f000fcb98c0c
sebacruz1 <sebastiancruzpomar@gmail.com>
2026-05-09 23:14:12 -0400

    Handler principal de conexion

---
+++ src/handlers/HandlerPrincipal.java
@@ -0,0 +1,52 @@
+package handlers;
+
+import modelos.Usuario;
+
+import java.io.*;
+import java.net.Socket;
+
+public class HandlerPrincipal implements Runnable {
+    private Socket socket;
+    private static int contadorUsuarios = 0;
+
+    private HandlerPrincipal(Socket socket) {
+        this.socket = socket;
+
+    }
+
+    @Override
+    public void run() {
+        try (
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());) {
+            String username = (String) in.readObject();
+            Usuario usuario = new Usuario(contadorUsuarios++, username);
+            usuario.setSesionActiva(true);
+
+            out.writeObject(usuario);
+            out.flush();
+
+            System.out.println("[Principal] Usuario conectado: " + username);
+
+            boolean activo = true;
+            while (activo) {
+                int opcion = (int) in.readObject();
+                switch (opcion) {
+                    case 1 -> out.writeObject("TIENDA");
+                    case 2 -> out.writeObject("MATCHMAKING");
+                    case 3 -> {
+                        out.writeObject("SALIR");
+                        activo = false;
+                    }
+                    default -> out.writeObject("ERROR");
+                }
+                out.flush();
+            }
+            System.out.println("[Principal] Usuario desconectado " + username);
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("[Principal] Cliente desconectado inesperadamente");
+        }
+    }
+
+}
